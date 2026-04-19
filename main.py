ebff86a0dcd9e7a68dbce3391394ddc4c5c22fe1
Vishesh Kathuria <vishesh.kathuria2004@gmail.com>
2026-04-20 01:54:43 +0530

    Refactor main.py to enhance command structure and add memory tracing functionality

--- main.py
+++ main.py
@@ -2,26 +2,30 @@ import typer
 import pathlib
 import dis
 from typing import Annotated
+import tracemalloc
 
 
-app = typer.Typer()
+app = typer.Typer(no_args_is_help=True)
 
+
+@app.command()
 def analyze(file:str):
     source = pathlib.Path(file).read_text('utf-8')
     code = compile(source,str(file),"exec")
     dis.dis(code)
-    
 
 
 @app.command()
-def main(
-    analyzer: Annotated[str,typer.Option()]
-):
-    if analyzer:
-        return analyze(analyzer)
-    
-
-
+def trace(file:str):
+    source = pathlib.Path(file).read_text('utf-8')
+    code = compile(source,str(file),"exec")
+    tracemalloc.start()
+    exec(code)
+    snapshot = tracemalloc.take_snapshot()
+    stats = snapshot.statistics('lineno')
+    tracemalloc.stop()
+    for stat in stats:
+        print(f"The status after tracing is -->{stat}")
 
 
 if __name__ == "__main__":
